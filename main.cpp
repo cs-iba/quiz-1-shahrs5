c966fd06af7e59ecbe5e29641907428e52f94dd7
shahrs5 <shahrs55555@gmail.com>
2024-09-16 13:02:58 +0500

    Added cchanges

--- main.cpp
+++ main.cpp
@@ -5,43 +5,67 @@ using namespace std;
 
 // This function performs a stable selection sort on a vector of any type.
 // `pos` specifies the index to sort by if sorting a vector of pairs.
+
+// First when finding the minimum element, we need to find the leftmost entry, second instead of moving the minimum element to the front,
+// we need to move all the elements to the left then add the minimum element
 template <typename T>
-void stableSelectionSort(vector<T>& a) {
-    
+void stableSelectionSort(vector<T> &a)
+{
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j].first < a[min].first)
+                min = j;
+        }
+        std::swap(a[i], a[min]);
+        int j = i;
+    }
 }
 
 // Unstable Selection Sort given in slides
 template <typename T>
-void selectionSort(vector<T>& a) {
-     int n = a.size();
-        for (int i = 0; i < n; i++) {
-            int min = i;
-            for (int j = i+1; j < n; j++) {
-                if (a[j] < a[min]) min = j;
-            }
-            std::swap(a[i], a[min]);
+void selectionSort(vector<T> &a)
+{
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[min])
+                min = j;
         }
+        std::swap(a[i], a[min]);
+    }
 }
-int main() {
+int main()
+{
     // Example usage with an array of pairs
     vector<pair<int, int>> arr = {{3, 1}, {2, 2}, {1, 3}, {2, 1}};
 
     // Print original array
     cout << "Original array:" << endl;
-    for (const auto& p : arr) {
+    for (const auto &p : arr)
+    {
         cout << "(" << p.first << ", " << p.second << ") ";
     }
     cout << endl;
 
     // Sort by the second element of the pairs
-    selectionSort(arr);
+    stableSelectionSort(arr);
 
     // Print sorted array
     cout << "Sorted array by the first element:" << endl;
-    for (const auto& p : arr) {
+    for (const auto &p : arr)
+    {
         cout << "(" << p.first << ", " << p.second << ") ";
     }
     cout << endl;
+
+
 
     return 0;
 }
